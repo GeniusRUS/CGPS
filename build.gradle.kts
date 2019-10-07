9bd5e5542bd8b6a56e1e412eb1eb594dde67fe91
VO Lixanov <volixanov@unitbean.com>
2019-10-07 08:13:49 +0400

    - обновлена зависимость android studio до 3.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0")
+        classpath("com.android.tools.build:gradle:3.5.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra.get("kotlin_version") as String}")
 
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
