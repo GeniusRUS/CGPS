962329a63ab13241ec171bc937b73e815e4d6ce7
Viktor Likhanov <volixanov@unitbean.io>
2021-02-14 19:56:32 +0300

    - Initial configuration deploy to Maven

--- cgps/build.gradle.kts
+++ cgps/build.gradle.kts
@@ -1,21 +1,14 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
-buildscript {
-    repositories {
-        jcenter()
-    }
-    dependencies {
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:${rootProject.extra.get("dokka_version") as String}")
-    }
-}
-
 plugins {
-    id("com.jfrog.bintray")
-    id("maven-publish")
     id("com.android.library")
     kotlin("android")
-    id("org.jetbrains.dokka") version "1.4.20"
+    id("org.jetbrains.dokka")
+    id("com.vanniktech.maven.publish") version "0.13.0"
+}
+
+tasks.dokkaJavadoc.configure {
+    outputDirectory.set(buildDir.resolve("javadoc"))
 }
 
 android {
@@ -65,104 +58,4 @@ dependencies {
     androidTestImplementation("androidx.test:runner:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
     androidTestImplementation("androidx.test:rules:1.3.0")
-}
-
-group = "com.geniusrus.cgps"
-version = android.defaultConfig.versionName.toString()
-
-tasks {
-    dokkaJavadoc.configure {
-        outputDirectory.set(buildDir.resolve("javadoc"))
-        dokkaSourceSets {
-            configureEach {
-                sourceLink {
-                    localDirectory.set(file("src/main/java"))
-                    remoteUrl.set(uri("https://github.com/GeniusRUS/CGPS/tree/master/cgps/src/main/java").toURL())
-                    remoteLineSuffix.set("#L")
-                }
-            }
-        }
-    }
-
-    register("androidJavadocJar", Jar::class) {
-        archiveClassifier.set("javadoc")
-        from("$buildDir/javadoc")
-        dependsOn(dokkaJavadoc)
-    }
-
-    register("androidSourcesJar", Jar::class) {
-        archiveClassifier.set("sources")
-        from(android.sourceSets.getByName("main").java.srcDirs)
-    }
-}
-
-publishing {
-    publications {
-        register<MavenPublication>("CGPSLibrary") {
-            artifactId = "cgps"
-
-            afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
-            artifact(tasks.getByName("androidJavadocJar"))
-            artifact(tasks.getByName("androidSourcesJar"))
-
-            pom {
-                name.set("CGPS")
-                description.set("Android location library on coroutines")
-                url.set("https://github.com/GeniusRUS/CGPS")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("GeniusRUS")
-                        name.set("Viktor Likhanov")
-                        email.set("Gen1usRUS@yandex.ru")
-                    }
-                }
-                scm {
-                    connection.set("git://github.com/GeniusRUS/CGPS.git")
-                    developerConnection.set("git://github.com/GeniusRUS/CGPS.git")
-                    url.set("https://github.com/GeniusRUS/CGPS")
-                }
-
-                // Saving external dependencies list into .pom-file
-                withXml {
-                    fun groovy.util.Node.addDependency(dependency: Dependency, scope: String) {
-                        appendNode("dependency").apply {
-                            appendNode("groupId", dependency.group)
-                            appendNode("artifactId", dependency.name)
-                            appendNode("version", dependency.version)
-                            appendNode("scope", scope)
-                        }
-                    }
-
-                    asNode().appendNode("dependencies").let { dependencies ->
-                        // List all "api" dependencies as "compile" dependencies
-                        configurations.api.get().allDependencies.forEach {
-                            dependencies.addDependency(it, "compile")
-                        }
-                        // List all "implementation" dependencies as "runtime" dependencies
-                        configurations.implementation.get().allDependencies.forEach {
-                            dependencies.addDependency(it, "runtime")
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            name = "bintray"
-            credentials {
-                username = gradleLocalProperties(rootDir).getProperty("bintray.user")
-                password = gradleLocalProperties(rootDir).getProperty("bintray.apikey")
-            }
-            url = uri("https://api.bintray.com/maven/geniusrus/CGPS/$group/;publish=1")
-        }
-    }
 }
