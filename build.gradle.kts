d3bc80fadf6dd087c776417f5ed71c127adf197d
Viktor Likhanov <volixanov@unitbean.io>
2023-04-14 14:01:11 +0300

    - refactor: updating kotlin version - refactor: updating gradle wrapper version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.0.0" apply false
     id("com.android.library") version "8.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
     id("org.jetbrains.dokka") version "1.8.10" apply false
     id("com.vanniktech.maven.publish") version "0.25.1" apply false
     id("com.huawei.agconnect.agcp") version "1.8.1.300" apply false
